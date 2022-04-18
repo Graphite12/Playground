eeae02bbe01873b5bb4c9caa3bc1d37a2e04a99c
Elitebook <elitebook@Elitebookui-MacBookAir.local>
2022-04-19 00:53:26 +0900

    회원가입이후, 로그인 구현

--- NodeJS/bin/core/routes/user/userRouter.js
+++ NodeJS/bin/core/routes/user/userRouter.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 import userController from './controllers/userController.js';
 const userRouter = Router();
 
+/* 로그인, 가입 폼 가져오기 */
 userRouter.get('/signin', userController.getLoginForm);
 userRouter.get('/signup', userController.getRegisterForm);
-userRouter.post('/signup', userController.signUp);
+
+/* 로그인, 가입 데이터 DB전달 */
 userRouter.post('/signin', userController.signIn);
+userRouter.post('/signup', userController.signUp);
+
+/* 기타 등등 */
 userRouter.get('/:id/profile');
 userRouter.get('/logout');
 
